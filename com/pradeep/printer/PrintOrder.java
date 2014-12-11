4b6f91ccc04d5767496e7d8f9d9152246663422f
pradeep singh <er.pradeep007@gmail.com>
2014-12-11 11:59:06 +0530

    PrintOrder.java
    Printer handler for printing

---
+++ com/pradeep/printer/PrintOrder.java
@@ -0,0 +1,24 @@
+package com.pradeep.printer;
+
+import java.io.IOException;
+import android.content.Context;
+import com.pradeep.adapter.USBAdapter;
+
+public class PrintOrder {
+	
+	public PrintOrder(){
+		
+	}
+	
+	public void Print(Context context,String msg){
+		USBAdapter usba = new USBAdapter();
+			usba.createConn(context);
+		try {
+			usba.openPort(context,msg);
+			usba.closeConnection(context);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
